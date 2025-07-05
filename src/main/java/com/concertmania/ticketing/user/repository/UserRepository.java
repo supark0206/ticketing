68fb5f6a31a280d26ac40052545c5df5b0b486ad
supark0206 <supark0206@gmail.com>
2025-07-05 15:48:23 +0900

    [feat] 서버 실행시 Default admin 자동 생성 기능 구현

--- src/main/java/com/concertmania/ticketing/user/repository/UserRepository.java
+++ src/main/java/com/concertmania/ticketing/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.concertmania.ticketing.user.repository;
 
 import com.concertmania.ticketing.user.entity.User;
+import com.concertmania.ticketing.user.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    long countByRole(UserRole role);
 }
